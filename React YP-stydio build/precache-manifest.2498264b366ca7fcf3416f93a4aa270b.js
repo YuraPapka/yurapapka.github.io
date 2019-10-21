af449b51f9f45a9c23ec79cef6a893ca478d5cd6
Yurii <papka.yurii@gmail.com>
2019-10-21 20:41:42 +0300

    add react app
    add build react app/YP-stydio, just for you to look

---
+++ React YP-stydio build/precache-manifest.2498264b366ca7fcf3416f93a4aa270b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "130a1fb8560e2b722f126c5bedd801f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "777fad31acb990a67e59",
+    "url": "/static/css/main.9c4030f6.chunk.css"
+  },
+  {
+    "revision": "8ab64572ac780256120a",
+    "url": "/static/js/2.ab26591e.chunk.js"
+  },
+  {
+    "revision": "777fad31acb990a67e59",
+    "url": "/static/js/main.68098807.chunk.js"
+  },
+  {
+    "revision": "4064b2a89ece504ae218",
+    "url": "/static/js/runtime-main.59106e71.js"
+  },
+  {
+    "revision": "3f05a5510e13712f6682f933b0801534",
+    "url": "/static/media/bg.3f05a551.jpg"
+  }
+]);
