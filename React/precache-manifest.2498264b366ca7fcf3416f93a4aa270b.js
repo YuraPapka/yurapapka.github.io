b880e4a4bb7f17ec41bc6d54289ebf3430984935
Yurii <papka.yurii@gmail.com>
2019-10-21 21:22:03 +0300

    fix path to files

--- React/precache-manifest.2498264b366ca7fcf3416f93a4aa270b.js
+++ React/precache-manifest.2498264b366ca7fcf3416f93a4aa270b.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "777fad31acb990a67e59",
-    "url": "/static/css/main.9c4030f6.chunk.css"
+    "url": "/React/static/css/main.9c4030f6.chunk.css"
   },
   {
     "revision": "8ab64572ac780256120a",
-    "url": "/static/js/2.ab26591e.chunk.js"
+    "url": "/React/static/js/2.ab26591e.chunk.js"
   },
   {
     "revision": "777fad31acb990a67e59",
-    "url": "/static/js/main.68098807.chunk.js"
+    "url": "/React/static/js/main.68098807.chunk.js"
   },
   {
     "revision": "4064b2a89ece504ae218",
-    "url": "/static/js/runtime-main.59106e71.js"
+    "url": "/React/static/js/runtime-main.59106e71.js"
   },
   {
     "revision": "3f05a5510e13712f6682f933b0801534",
-    "url": "/static/media/bg.3f05a551.jpg"
+    "url": "/React/static/media/bg.3f05a551.jpg"
   }
 ]);
